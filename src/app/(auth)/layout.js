b64d6a4cabf526ffd1fbfca5b338844f8db25bb8
wijayanto1320 <wijayanto1320@gmail.com>
2024-07-28 06:53:20 +0700

    delete comment at register/layout.js

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,12 +1,4 @@
-import { auth } from "@/libs/auth";
-import { redirect } from "next/navigation";
-
 export default async function Layout({ children }) {
-  //   const user = auth();
-
-  //   if (user) {
-  //     redirect("/");
-  //   }
 
   return (
     <main>
