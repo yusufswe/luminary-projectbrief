45ef21a3ec1f3daefd6e3a389378a54e966959ce
wijayanto1320 <wijayanto1320@gmail.com>
2024-07-28 00:45:02 +0700

    final5

--- src/app/bookmark/components/getBrief.js
+++ src/app/bookmark/components/getBrief.js
@@ -1,11 +1,12 @@
-"use server";
-
 import { getProjectBriefWithDetailsById } from "../actionGetBrief";
 import { Brief } from "./briefslist";
 
 
 const GetBrief = async() => {
   const briefs = await getProjectBriefWithDetailsById();
+  if (!briefs) {
+    redirect("/");
+  }
 
   return (
     <div className="w-full">
